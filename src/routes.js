093a75c3d576fbb2d1115d1f2381f285c9115acc
Igor Gomes <igor.soares@cromai.com>
2020-03-28 03:03:24 -0300

    Creation of Backend

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const SessionsController = require('./controllers/SessionController')
+const OngController = require('./controllers/OngController')
+const IncidentsController = require('./controllers/IncidentsController')
+const ProfileController = require('./controllers/ProfileController')
+
+const routes = express.Router()
+
+routes.post('/sessions', SessionsController.create)
+
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
+
+routes.get('/incidents', IncidentsController.index)
+routes.post('/incidents', IncidentsController.create)
+routes.delete('/incidents/:id', IncidentsController.delete)
+
+routes.get('/profile', ProfileController.index)
+
+module.exports = routes
